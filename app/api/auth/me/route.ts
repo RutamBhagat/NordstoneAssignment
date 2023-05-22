4db67298b12f5e01ed1caa0cd0a31549a247ffa7
RutamBhagat <rutambhagat@gmail.com>
2023-05-22 23:07:52 +0530

    fix endpoint

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -25,7 +25,6 @@ export async function GET(request: NextRequest, response: NextResponse) {
       city: true,
       email: true,
       phone: true,
-      reviews: true,
       created_at: true,
       updated_at: true,
     },
