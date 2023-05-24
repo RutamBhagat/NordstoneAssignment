9eb4173006cac3bcbed4767802e44ded67e2df0b
RutamBhagat <rutambhagat@gmail.com>
2023-05-25 02:18:24 +0530

    remove swiper

--- app/api/photos/update/route.ts
+++ app/api/photos/update/route.ts
@@ -6,14 +6,19 @@ export async function POST(request: NextRequest) {
   const { id, newUrl } = body;
 
   // Update a image url
+  console.log("THIS IS HIT 1");
   try {
+    console.log("THIS IS HIT 2");
     const result = await prisma.photo.update({
       where: { id: id },
       data: { url: newUrl },
     });
+    console.log("result", result);
+    console.log("THIS IS HIT 3");
 
     return NextResponse.json(result, { status: 200 });
   } catch (error) {
+    console.log("THIS IS HIT ERROR");
     console.log("error", error);
     return NextResponse.json({ err: "Error occured while updating an image url" }, { status: 400 });
   }
