06789537de5cbbdd36158eba585bc35a8385d1df
RutamBhagat <rutambhagat@gmail.com>
2023-05-25 17:01:02 +0530

    improve calculator logic

--- app/(page group)/calculator/page.tsx
+++ app/(page group)/calculator/page.tsx
@@ -2,157 +2,197 @@
 import React, { useState } from "react";
 
 export default function page() {
-  const [result, setResult] = useState("");
+  const [result, setResult] = useState("0");
 
   const handleClick = (e: any) => {
-    setResult(result.concat(e.target.name));
+    const num = e.target.name;
+
+    if (result === "0" && !isNaN(parseInt(num))) {
+      setResult(num);
+      return;
+    }
+
+    let ch = result;
+    if (isNaN(parseInt(ch[ch.length - 1])) && isNaN(parseInt(num))) {
+      if (ch.length <= 1) {
+        ch = "0";
+      }
+      ch = ch.slice(0, -1);
+    }
+
+    setResult(ch.concat(num));
   };
+
   const clear = () => {
-    setResult("");
+    setResult("0");
   };
+
   const backspace = () => {
-    setResult(result.slice(0, -1)); // Or -1
+    let ch = result;
+    if (ch.length <= 1) {
+      setResult("0");
+      return;
+    }
+    ch = ch.slice(0, -1);
+    if (ch.length === 1 && isNaN(parseInt(ch))) {
+      ch = "0";
+    }
+    setResult(ch);
   };
+
   const calculate = () => {
+    let ch = result;
+    if (isNaN(parseInt(ch[ch.length - 1]))) {
+      if (ch.length <= 1) {
+        ch = "0";
+      }
+      ch = ch.slice(0, -1);
+    }
+
     try {
-      setResult(eval(result).toString());
+      let evaluation = eval(ch).toString();
+      if (evaluation === "NaN") {
+        evaluation = "Cannot divide by zero";
+      }
+      setResult(evaluation);
     } catch (err) {
       setResult("Error");
     }
   };
+
   return (
     <div className="min-h-screen flex justify-center items-center">
-      <div className="absolute -translate-x-2/4 -translate-y-2/4 w-64 text-center mx-auto my-0 rounded-[5px] border-[10px] border-solid border-[#101116] left-2/4 top-2/4 bg[#101116]">
+      <div className="text-xl p-3 mt-[60px] w-full max-w-xl rounded-[5px] border border-gray-900 bg-gray-900">
         <form>
           <input
-            className="outline-none h-[75px] w-full bg-[#101116] text-white text-right text-[25px] font-medium tracking-[1px] border-0"
+            className="outline-none h-[100px] w-full bg-gray-900 text-white text-right text-[25px] font-medium tracking-[1px] border-0"
             type="text"
             value={result}
           />
         </form>
 
-        <div className="grid grid-cols-4 bg-[#101116] auto-rows-[minmax(60px,auto)]">
+        <div className="grid grid-cols-4 auto-rows-[minmax(90px,auto)]">
           <button
-            className="outline-none cursor-pointer font-medium m-[5px] rounded-[5px] border-[none] bg-[#56cbdb] col-span-2 text-white"
+            className="outline-none cursor-pointer font-medium m-[5px] rounded-[5px] border-[none] bg-cyan-600 col-span-2 text-white"
             onClick={clear}
             id="clear"
           >
             Clear
           </button>
           <button
-            className="outline-none cursor-pointer font-medium m-[5px] rounded-[5px] border-[none] bg-[#56cbdb] text-white"
+            className="outline-none cursor-pointer font-medium m-[5px] rounded-[5px] border-[none] bg-cyan-600 text-white"
             onClick={backspace}
             id="backspace"
           >
             C
           </button>
           <button
-            className="outline-none cursor-pointer text-white font-medium m-[5px] rounded-[5px] border-[none] bg-[#56cbdb]"
+            className="outline-none cursor-pointer text-white font-medium m-[5px] rounded-[5px] border-[none] bg-cyan-600"
             onClick={handleClick}
             name="/"
           >
             &divide;
           </button>
           <button
-            className="outline-none cursor-pointer text-white font-medium m-[5px] rounded-[5px] border-[none] bg-[#262834]"
+            className="outline-none cursor-pointer text-white font-medium m-[5px] rounded-[5px] border-[none] bg-gray-800"
             onClick={handleClick}
             name="7"
           >
             7
           </button>
           <button
-            className="outline-none cursor-pointer text-white font-medium m-[5px] rounded-[5px] border-[none] bg-[#262834]"
+            className="outline-none cursor-pointer text-white font-medium m-[5px] rounded-[5px] border-[none] bg-gray-800"
             onClick={handleClick}
             name="8"
           >
             8
           </button>
           <button
-            className="outline-none cursor-pointer text-white font-medium m-[5px] rounded-[5px] border-[none] bg-[#262834]"
+            className="outline-none cursor-pointer text-white font-medium m-[5px] rounded-[5px] border-[none] bg-gray-800"
             onClick={handleClick}
             name="9"
           >
             9
           </button>
           <button
-            className="outline-none cursor-pointer text-white font-medium m-[5px] rounded-[5px] border-[none] bg-[#56cbdb]"
+            className="outline-none cursor-pointer text-white font-medium m-[5px] rounded-[5px] border-[none] bg-cyan-600"
             onClick={handleClick}
             name="*"
           >
             &times;
           </button>
           <button
-            className="outline-none cursor-pointer text-white font-medium m-[5px] rounded-[5px] border-[none] bg-[#262834]"
+            className="outline-none cursor-pointer text-white font-medium m-[5px] rounded-[5px] border-[none] bg-gray-800"
             onClick={handleClick}
             name="4"
           >
             4
           </button>
           <button
-            className="outline-none cursor-pointer text-white font-medium m-[5px] rounded-[5px] border-[none] bg-[#262834]"
+            className="outline-none cursor-pointer text-white font-medium m-[5px] rounded-[5px] border-[none] bg-gray-800"
             onClick={handleClick}
             name="5"
           >
             5
           </button>
           <button
-            className="outline-none cursor-pointer text-white font-medium m-[5px] rounded-[5px] border-[none] bg-[#262834]"
+            className="outline-none cursor-pointer text-white font-medium m-[5px] rounded-[5px] border-[none] bg-gray-800"
             onClick={handleClick}
             name="6"
           >
             6
           </button>
           <button
-            className="outline-none cursor-pointer text-white font-medium m-[5px] rounded-[5px] border-[none] bg-[#56cbdb]"
+            className="outline-none cursor-pointer text-white font-medium m-[5px] rounded-[5px] border-[none] bg-cyan-600"
             onClick={handleClick}
             name="-"
           >
             &ndash;
           </button>
           <button
-            className="outline-none cursor-pointer text-white font-medium m-[5px] rounded-[5px] border-[none] bg-[#262834]"
+            className="outline-none cursor-pointer text-white font-medium m-[5px] rounded-[5px] border-[none] bg-gray-800"
             onClick={handleClick}
             name="1"
           >
             1
           </button>
           <button
-            className="outline-none cursor-pointer text-white font-medium m-[5px] rounded-[5px] border-[none] bg-[#262834]"
+            className="outline-none cursor-pointer text-white font-medium m-[5px] rounded-[5px] border-[none] bg-gray-800"
             onClick={handleClick}
             name="2"
           >
             2
           </button>
           <button
-            className="outline-none cursor-pointer text-white font-medium m-[5px] rounded-[5px] border-[none] bg-[#262834]"
+            className="outline-none cursor-pointer text-white font-medium m-[5px] rounded-[5px] border-[none] bg-gray-800"
             onClick={handleClick}
             name="3"
           >
             3
           </button>
           <button
-            className="outline-none cursor-pointer text-white font-medium m-[5px] rounded-[5px] border-[none] bg-[#56cbdb]"
+            className="outline-none cursor-pointer text-white font-medium m-[5px] rounded-[5px] border-[none] bg-cyan-600"
             onClick={handleClick}
             name="+"
           >
             +
           </button>
           <button
-            className="outline-none cursor-pointer text-white font-medium m-[5px] rounded-[5px] border-[none] bg-[#262834]"
+            className="outline-none cursor-pointer text-white font-medium m-[5px] rounded-[5px] border-[none] bg-gray-800"
             onClick={handleClick}
             name="0"
           >
-            @
+            0
           </button>
           <button
-            className="outline-none cursor-pointer text-white font-medium m-[5px] rounded-[5px] border-[none] bg-[#262834]"
+            className="outline-none cursor-pointer text-white font-medium m-[5px] rounded-[5px] border-[none] bg-gray-800"
             onClick={handleClick}
             name="."
           >
             .
           </button>
           <button
-            className="outline-none cursor-pointer font-medium m-[5px] rounded-[5px] border-[none] bg-[#56cbdb] col-span-2 text-white"
+            className="outline-none cursor-pointer font-medium m-[5px] rounded-[5px] border-[none] bg-cyan-600 col-span-2 text-white"
             onClick={calculate}
             id="result"
           >
