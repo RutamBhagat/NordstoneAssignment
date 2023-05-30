03712234809be60e5c0a7ec8627527cc4e1ede97
RutamBhagat <rutambhagat@gmail.com>
2023-05-30 19:47:22 +0530

    remove unnecessary code

--- app/(page group)/text/page.tsx
+++ app/(page group)/text/page.tsx
@@ -3,16 +3,14 @@ import axios from "axios";
 import { AuthenticationContext } from "@/app/context/AuthContext";
 import { useQuery } from "@tanstack/react-query";
 import { useContext } from "react";
-import CreatePost from "./components/CreatePost";
 import ErrorComponent from "../components/ErrorComponent";
-import LoadingComponent from "../components/LoadingComponent";
 import Posts from "./components/Posts";
 
 export default function page() {
   const auth = useContext(AuthenticationContext);
 
   if (!auth.data?.email) {
-    return <LoadingComponent />;
+    return <></>;
   }
 
   const { data, error, isError, isLoading } = useQuery<PostType[]>({
@@ -24,7 +22,7 @@ export default function page() {
   });
 
   if (isLoading) {
-    return <LoadingComponent />;
+    return <></>;
   }
 
   if (isError) {
@@ -33,23 +31,10 @@ export default function page() {
   }
 
   return (
-    <div className="flex h-screen text-gray-800">
-      <div className="flex flex-row h-full w-full overflow-x-hidden">
-        <div className="flex flex-col flex-auto pt-[60px]">
-          <div className="flex flex-col flex-auto flex-shrink-0 bg-gray-100 h-full p-4">
-            <div className="flex flex-col h-full overflow-x-auto mb-4">
-              <div className="flex flex-col h-full">
-                <div className="grid grid-cols-12 gap-y-2">
-                  {data?.map((post) => (
-                    <Posts key={post.id} post={post} />
-                  ))}
-                </div>
-              </div>
-            </div>
-            <CreatePost />
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+      {data?.map((post) => (
+        <Posts key={post.id} post={post} />
+      ))}
+    </>
   );
 }
